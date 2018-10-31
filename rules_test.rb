af9596c3bbf0defba9acf4755edf07f4967c1bfa
MustafaPitt <mua31@pitt.edu>
2018-10-31 19:26:58 -0400

    fix typo

--- rules_test.rb
+++ rules_test.rb
@@ -1,11 +1,9 @@
 require_relative 'rules'
 require 'minitest/autorun'
 
-  # testing Rules class
+# testing Rules class
 
 class RulesTest < Minitest::Test
-
-
   # UNIT TESTS FOR METHOD pos_int?(input)
   # Equivalence classes:
   # input = 0..INFINITY -> returns true
@@ -14,7 +12,7 @@ class RulesTest < Minitest::Test
 
   # If a negative value is given for input , then false is returned.
   def test_negative_val
-    set_rules = Rules::new
+    set_rules = Rules.new
     input = '-11'
     actual = set_rules.pos_int? input
     expected = false
@@ -38,7 +36,6 @@ class RulesTest < Minitest::Test
     assert_equal expected, actual
   end
 
-
   # if invalid value, such as float number , is given for input then false is returned
   # test_float_number
   # # EDGE CASE
@@ -56,12 +53,13 @@ class RulesTest < Minitest::Test
 
   # If a negative value is given for input , then false is returned.
   def test_neg_val
-    set_rules = Rules::new
+    set_rules = Rules.new
     input = '-11'
     actual = set_rules.pos_neg_int? input
     expected = true
     assert_equal expected, actual
   end
+
   # If a positive value is given for input , then true is returned.
   def test_pos_val
     set_rule = Rules.new
@@ -89,21 +87,23 @@ class RulesTest < Minitest::Test
   # if seed is positive and number of prospectors are positive, then true returned
   def test_pos_seed_pos_pros
     set_rule = Rules.new
-    actual = set_rule.valid_input? '123','453'
+    actual = set_rule.valid_input? '123', '453'
     expected = true
     assert_equal expected, actual
   end
+
   # if seed is negative and number of prospectors are positive, then true returned
   def test_neg_seed_pos_pros
     set_rule = Rules.new
-    actual = set_rule.valid_input? '-123','453'
+    actual = set_rule.valid_input? '-123', '453'
     expected = true
     assert_equal expected, actual
   end
+
   # if seed is negative and number of prospectors is negative, then false returned
   def test_neg_seed_neg_pros
     set_rule = Rules.new
-    actual = set_rule.valid_input? '-123','-453'
+    actual = set_rule.valid_input? '-123', '-453'
     expected = false
     assert_equal expected, actual
   end
@@ -111,11 +111,10 @@ class RulesTest < Minitest::Test
   # if seed is negative and number of prospectors is negative, then false returned
   def test_pros_seed_invalid
     set_rule = Rules.new
-    actual = set_rule.valid_input? '-323','123A'
+    actual = set_rule.valid_input? '-323', '123A'
     expected = false
     assert_equal expected, actual
   end
-
 
   # UNIT TESTS FOR METHOD first_3_cities(id, prng, pross)
   # The method should stop after visit 3 cities
@@ -124,15 +123,14 @@ class RulesTest < Minitest::Test
     map = Map.new
     set_rule = Rules.new
     prng = Random.new 12
-    id =0
-    prospectors =[]
+    id = 0
+    prospectors = []
     prospectors[id] = Prospector.new id, prng
     prospectors[id].current_city = map.cities[2]
 
-    set_rule.first_3_cities id, prng,prospectors
+    set_rule.first_3_cities id, prng, prospectors
 
-    assert_equal 3 ,prospectors[id].visited_num
-
+    assert_equal 3, prospectors[id].visited_num
   end
   # UNIT TESTS FOR METHOD final_2_cities(id, prng, pross)
   # The method should stop after visit 5 cities
@@ -141,14 +139,12 @@ class RulesTest < Minitest::Test
     map = Map.new
     set_rule = Rules.new
     prng = Random.new 12
-    id =0
-    prospectors =[]
+    id = 0
+    prospectors = []
     prospectors[id] = Prospector.new id, prng
     prospectors[id].current_city = map.cities[2]
 
-    set_rule.final_2_cities id, prng,prospectors
-    assert_equal 5 ,prospectors[id].visited_num
-
+    set_rule.final_2_cities id, prng, prospectors
+    assert_equal 5, prospectors[id].visited_num
   end
-
 end
